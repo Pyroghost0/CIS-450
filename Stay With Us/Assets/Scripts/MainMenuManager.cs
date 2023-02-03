b349d733c7aa68e3f6e815953aac04259ac9e44a
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2023-02-03 13:38:32 -0600

    Added Unity Game

---
+++ Stay With Us/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+/* Caleb Kahn
+ * MainMenuManager
+ * Project 1
+ * UI for main menu
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Night 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
