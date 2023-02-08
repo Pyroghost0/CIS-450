497a002ee558be2f6d41136446fea26bd3af9d2f
abreuker <abreuker@mail.bradley.edu>
2023-02-08 14:45:39 -0600

    Update Grave.cs

--- Stay With Us/Assets/Scripts/Grave.cs
+++ Stay With Us/Assets/Scripts/Grave.cs
@@ -16,13 +16,15 @@ public class Grave : MonoBehaviour
     //going to take the location of the player for proximity
     //or we could use observer pattern if we want to make it
     //overcomplicated but get the design pattern out of the way.
-    public PlayerMovement player;
+    
+    //public PlayerMovement player;
 
     //unless we want to do colliders, in which case that's just 
     //an on trigger enter 2D
 
     //for the ghost ai
-    public Ghost ghost; 
+    
+    //public Ghost ghost; 
 
     // Start is called before the first frame update
     void Start()
