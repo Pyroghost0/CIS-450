9d83d7b1fa6d9fca9f963cb04d0a86feb1d71dfe
CooperCreeper18 <100331876+CooperCreeper18@users.noreply.github.com>
2023-02-09 18:23:57 -0600

    added simple player movement and ability buttons

--- Stay With Us/Assets/Scripts/Grave.cs
+++ Stay With Us/Assets/Scripts/Grave.cs
@@ -16,23 +16,29 @@ public class Grave : MonoBehaviour
     //going to take the location of the player for proximity
     //or we could use observer pattern if we want to make it
     //overcomplicated but get the design pattern out of the way.
+
     //public Player player;
+
+    
+    //public PlayerMovement player;
+
 
     //unless we want to do colliders, in which case that's just 
     //an on trigger enter 2D
 
     //for the ghost ai
+
     //public Ghost ghost; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
