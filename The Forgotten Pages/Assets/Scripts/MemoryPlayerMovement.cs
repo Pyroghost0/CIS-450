1bbcacd539c268ec6b6a74ce271b4ac1645443f7
abreuker <abreuker@mail.bradley.edu>
2023-04-02 16:19:31 -0500

    movement and camera set up in memory test scene

---
+++ The Forgotten Pages/Assets/Scripts/MemoryPlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryPlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float horizontal;
+    public float moveLimiter = 0.7f;
+    public float movementSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assign variables
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //player movement
+        horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0)
+        {
+            horizontal *= moveLimiter;
+        }
+        body.velocity = new Vector3(horizontal * movementSpeed, 0);
+    }
+}
