3b7cd1bc3e63c9e30a2ca7841c640af9d1508da1
abreuker <abreuker@mail.bradley.edu>
2023-04-05 12:28:06 -0500

    added some 3d assets

---
+++ The Forgotten Pages/Assets/Scripts/MemoryNPC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryNPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: method for movement, method for talking. 
+    //      should make a controller to be able to script these cutscenes better.
+    //      could use a design pattern to get this done.
+}
