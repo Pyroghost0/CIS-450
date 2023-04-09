e17dcdf851d8b8f84b75712b67ac5329417fd1a5
abreuker <abreuker@mail.bradley.edu>
2023-04-09 18:34:46 -0500

    basic npc movement and dialogue working for cutscenes

---
+++ The Forgotten Pages/Assets/Scripts/Memory1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory1 : MonoBehaviour
+{
+    public MemoryNPC motherCharacter;
+    public MemoryNPC importantKidCharacter;
+    public MemoryNPC[] otherChildren;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- The Forgotten Pages/Assets/Scripts/MemoryNPC.cs
+++ The Forgotten Pages/Assets/Scripts/MemoryNPC.cs
@@ -1,19 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder;
+using TMPro;
 
 public class MemoryNPC : MonoBehaviour
 {
+    public string[] dialouge;
+    public GameObject textbox;
+    public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartDialouge(new string[] {"This is the placeholder text!", "woo!", "woooooo!" });
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveToPosition(-3, 3);
+    }
+
+    public void MoveToPosition(float newPos, float speed)
+    {
+        if (gameObject.transform.position.x < newPos - .1f || gameObject.transform.position.x > newPos + .1f)
+        {
+            if (gameObject.transform.position.x < newPos)
+            {
+                gameObject.transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+            }
+            else if (gameObject.transform.position.x > newPos)
+            {
+                gameObject.transform.position -= new Vector3(Time.deltaTime * speed, 0, 0);
+            }
+        }
+    }
+
+    public void MoveToRandomPosition(float maxPos, float minPos, float speed)
+    { 
         
+    }
+
+    public void StartDialouge(string[] newDialouge)
+    { 
+        dialouge = newDialouge;
+        StartCoroutine(Talk());
+    }
+
+    public IEnumerator Talk()
+    {
+        textbox.SetActive(true);
+        text.text = dialouge[0];
+        for (int i = 1; i < dialouge.Length; i++)
+        {
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            Debug.Log(i);
+            text.text = dialouge[i];
+            yield return new WaitForSeconds(.5f);
+        }
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        textbox.SetActive(false);
     }
 
     //TODO: method for movement, method for talking. 
