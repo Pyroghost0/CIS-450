6f8e7395f780bb06140616b781b546c1f15ce874
abreuker <abreuker@mail.bradley.edu>
2023-05-01 12:28:49 -0500

    all art and animations for the car scene are in

---
+++ The Forgotten Pages/Assets/Scripts/Memory4.cs
@@ -0,0 +1,74 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Memory4 : MonoBehaviour, Memory
+{
+    public MemoryNPC motherCharacter;
+public MemoryTutorialBox tutorialBox;
+public MemoryExitDoor exitDoor;
+
+public GameObject player;
+public Vector3 playerStartPos;
+
+public PolygonCollider2D memoryConfiner;
+public CinemachineVirtualCamera cmCamera;
+
+public PlayerMovement player3D;
+
+// Start is called before the first frame update
+void Start()
+{
+    //StartCutscene();
+    player3D = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+
+public void StartCutscene()
+{
+    StartCoroutine(Cutscene());
+    cmCamera.GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = memoryConfiner;
+    player.transform.position = playerStartPos;
+}
+
+public IEnumerator Cutscene()
+{
+    yield return new WaitUntil(() => GameController.instance.isInMemory);
+
+    player.GetComponent<MemoryPlayerMovement>().canMove = false;
+    yield return new WaitForSeconds(.1f);
+
+    motherCharacter.moveToNewPos = true;
+    motherCharacter.newPos = 97.91f;
+    motherCharacter.speed = 2;
+
+    motherCharacter.StartDialouge(new string[] { "Oh, so here's where you ran off to!", "Looking for Dad?", "He told me he'd meet us back at home.", "Come on honey, let's go, the sun's almost set.", "We can pick up food on the way home." });
+    yield return new WaitUntil(() => !motherCharacter.isTalking);
+
+    motherCharacter.moveToNewPos = true;
+    motherCharacter.newPos = 104.5f;
+    motherCharacter.speed = 2;
+
+    player.GetComponent<MemoryPlayerMovement>().canMove = true;
+
+    yield return new WaitUntil(() => GameController.instance.isInMemory == false);
+
+
+    GameController.instance.memoriesCollected++;
+    Debug.Log(GameController.instance.memoriesCollected);
+
+}
+
+public void RecordMemory()
+{
+    player.GetComponent<MemoryPlayerMovement>().flashlightUnlocked = true;
+}
+}
+
